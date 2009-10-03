1f9c6ab83200f5e5de9d71694b415bd859d025cf
Alexander Wirt <formorer@debian.org>
2009-10-03 20:56:28 +0200

    Fix utf8 screwup

--- lib/Paste.pm
+++ lib/Paste.pm
@@ -29,7 +29,7 @@ use Carp;
 use vars qw(@ISA @EXPORT);
 @ISA = qw(Exporter);
 
-@EXPORT = qw ( new add_paste delete_paste get_paste get_config_key);
+@EXPORT = qw (new add_paste delete_paste get_paste get_config_key);
 sub new {
 	my $invocant = shift;
 	my $class = ref($invocant) || $invocant;	
@@ -96,7 +96,7 @@ A string with \n or \r\n which represents the paste entry
 
 =item B<name>
 
-The name of the submitter, anonymousif empty
+The name of the submitter, anonymous if empty
 
 =item B<expire> 
 
