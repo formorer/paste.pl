d9c3d2a153c8ff71cd213462dc19379d112dce41
Alexander Wirt <formorer@formorer.de>
2010-01-08 09:01:59 +0100

    Use configfile for dbsettings

--- update-db.pl
+++ update-db.pl
@@ -1,8 +1,29 @@
 #!/usr/bin/perl 
 
 use DBI; 
+use Config::IniFiles;
 
-$dbh = DBI->connect("dbi:Pg:dbname=paste", "", "");
+
+my $config_file = 'paste.conf';
+
+my $config = Config::IniFiles->new( -file => $config_file );
+unless ($config) {
+	my $error = "$!\n";
+	$error .= join("\n", @Config::IniFiles::errors);
+	die "Could not load configfile '$config_file': $error";
+}
+
+
+
+my $dbname = $config->val('database', 'dbname') || die "Databasename not specified in config";
+my $dbuser = $config->val('database', 'dbuser') || die "Databaseuser not specified in config";
+my $dbpass = $config->val('database', 'dbpassword') || '';
+
+
+
+my $dbh = DBI->connect("dbi:Pg:dbname=$dbname", $dbuser, $dbpass,
+	    { RaiseError => 0, PrintError => 0}) or
+	    die "Could not connect to DB: " . $DBI::errstr;
 
 $rows_affected = $dbh->do(
 	                 "DELETE FROM LANG");
@@ -12,10 +33,7 @@ $sth = $dbh->prepare( '
 	INSERT INTO lang ("desc") VALUES (?) 
 	') or die $dbh->errstr;
 
-open (my $fh, '<', 'langs') or die "could not open langs: $!"; 
-
-while (my $l = <$fh>) {
+while (my $l = <>) {
 	chomp($l); 
 	$sth->execute($l); 
 }
- 
